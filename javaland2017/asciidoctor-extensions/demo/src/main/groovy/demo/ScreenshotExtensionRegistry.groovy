63294e1cb32117d8c212716c9e10cd9562a2d1d0
Stephan Classen <st.classen@gmx.ch>
2017-03-22 22:49:43 +0100

    javaland asciidoctor demo: start

---
+++ javaland2017/asciidoctor-extensions/demo/src/main/groovy/demo/ScreenshotExtensionRegistry.groovy
@@ -0,0 +1,11 @@
+package demo
+
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.extension.spi.ExtensionRegistry
+
+class ScreenshotExtensionRegistry implements ExtensionRegistry {
+
+    @Override
+    void register(Asciidoctor asciidoctor) {
+    }
+}
